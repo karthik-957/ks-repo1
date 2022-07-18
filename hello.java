30d774ca4ebf5adb2d574fce961a7a4501459a53
karthi <8465988998>
2022-07-18 23:36:20 +0530

    modfy by 01 03 06 lines

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 
-class hello()
+class hello()        from dev03
 {
-   public static void main()
- {                                                      system.out.println("hello from dev03");
-     "hello.java"				
+   public static void main()   from dev03
+ {                                                        system.out.println("hello from dev03");
+                                              
+         "hello.java"     from dev03	
 
 	 system.out.println("hello frm dev01");
 
